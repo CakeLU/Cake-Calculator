06bb010f070a5163ab281468fbe304571d3c407e
Salah Salman <sasalman@mail.lipscomb.edu>
2016-12-05 10:17:29 -0600

    Going to be fixing all of the broken things.. :(

--- app/src/main/java/com/example/ss2sa/cakescalculator/MainActivity.java
+++ app/src/main/java/com/example/ss2sa/cakescalculator/MainActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
