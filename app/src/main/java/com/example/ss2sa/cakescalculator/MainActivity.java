c20491119785339a9432278ca9b15f400b34561e
Salah Salman <sasalman@mail.lipscomb.edu>
2016-12-06 19:11:43 -0600

    Fixed negative button function.

--- app/src/main/java/com/example/ss2sa/cakescalculator/MainActivity.java
+++ app/src/main/java/com/example/ss2sa/cakescalculator/MainActivity.java
@@ -1,22 +1,23 @@
 package com.example.ss2sa.cakescalculator;
 
-import android.content.Intent;
 import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-
 import org.w3c.dom.Text;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView result;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        result = (TextView) findViewById(R.id.result_text);
     }
 
     static String operation = "";
@@ -41,35 +42,24 @@ public class MainActivity extends AppCompatActivity {
 
     // This function retrieves the currently displayed text
     public String getDisplayString() {
-
-        TextView result = (TextView) findViewById(R.id.result_text);
         CharSequence currentResult = result.getText();
         return currentResult.toString();
-
     }
 
     // This function retrieves the currently displayed int
     public int getDisplayInt() {
-
-        TextView result = (TextView) findViewById(R.id.result_text);
         CharSequence currentResult = result.getText();
         return Integer.parseInt(currentResult.toString());
-
     }
 
     // This function retrieves the currently displayed float
     public double getDisplayDouble() {
-
-        TextView result = (TextView) findViewById(R.id.result_text);
         CharSequence currentResult = result.getText();
         return Double.parseDouble(currentResult.toString());
-
     }
 
     // This function clears all numbers on screen and operations
     public void buttonACPressed(View view) {
-
-        TextView result = (TextView) findViewById(R.id.result_text);
         operation = "";
         operationPressed = false;
         isDecimal = false;
@@ -78,9 +68,6 @@ public class MainActivity extends AppCompatActivity {
 
     // This function performs the operation and stores the new result
     public void buttonEqualsPressed(View view) {
-
-        TextView result = (TextView) findViewById(R.id.result_text);
-
         if (operation.equals("+")) {
             String tmp = (String.valueOf(Double.parseDouble(stringTmpResult) + Double.parseDouble(getDisplayString())));
             if (tmp.substring(tmp.length() - 2, tmp.length()).equals(".0")) {
@@ -158,7 +145,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
         else {
-            TextView result = (TextView) findViewById(R.id.result_text);
             result.setText(getDisplayString() + ".");
             isDecimal = true;
         }
@@ -238,9 +224,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    // This function changes a postitive number to a negative number and vice versa
+    // This function changes a positive number to a negative number and vice versa
     public void buttonPlusMinusPressed(View view) {
-        TextView result = (TextView) findViewById(R.id.result_text);
 
         String tmp = getDisplayString();
 
@@ -292,9 +277,6 @@ public class MainActivity extends AppCompatActivity {
 
     // This function takes the square root of a number
     public void buttonSquareRootPressed(View view) {
-
-        TextView result = (TextView) findViewById(R.id.result_text);
-
         if (Integer.parseInt(getDisplayString()) < 0) {
             showAlert(view, "You can't take the square root of a negative number!");
         }
@@ -308,9 +290,6 @@ public class MainActivity extends AppCompatActivity {
 
     // This function takes the sine of a number as long as it is given in degrees
     public void buttonSinePressed(View view) {
-
-        TextView result = (TextView) findViewById(R.id.result_text);
-
         if ((getDisplayInt() < -360 || getDisplayInt() > 360) || (getDisplayDouble() < -360.0 || getDisplayDouble() > 360.0)) {
             showAlert(view, "You need to input an angle between -360 and 360 degrees!");
         }
@@ -323,9 +302,6 @@ public class MainActivity extends AppCompatActivity {
 
     // This function takes the cosine of a number as long as it is given in degrees
     public void buttonCosinePressed(View view) {
-
-        TextView result = (TextView) findViewById(R.id.result_text);
-
         if ((getDisplayInt() < -360 || getDisplayInt() > 360) || (getDisplayDouble() < -360.0 || getDisplayDouble() > 360.0)) {
             showAlert(view, "You need to input an angle between -360 and 360 degrees!");
         }
@@ -338,9 +314,6 @@ public class MainActivity extends AppCompatActivity {
 
     // This function takes the hyperbolic tangent of a number
     public void buttonHyperbolicTangentPressed(View view) {
-
-        TextView result = (TextView) findViewById(R.id.result_text);
-
         if ((getDisplayInt() < -360 || getDisplayInt() > 360) || (getDisplayDouble() < -360.0 || getDisplayDouble() > 360.0)) {
             showAlert(view, "You need to input an angle between -360 and 360 degrees!");
         }
@@ -354,9 +327,6 @@ public class MainActivity extends AppCompatActivity {
 
     // This function adds a 0 to the currently displayed number
     public void buttonZeroPressed(View view) {
-
-        TextView result = (TextView) findViewById(R.id.result_text);
-
         if (operationPressed) {
             result.setText("0");
             operationPressed = false;
@@ -375,9 +345,6 @@ public class MainActivity extends AppCompatActivity {
 
     // This function adds a 1 to the currently displayed number
     public void buttonOnePressed(View view) {
-
-        TextView result = (TextView) findViewById(R.id.result_text);
-
         // If an operation is in place, auto set to the number pressed
         if (operationPressed) {
             result.setText("1");
@@ -397,9 +364,6 @@ public class MainActivity extends AppCompatActivity {
 
     // This function adds a 2 to the currently displayed number
     public void buttonTwoPressed(View view) {
-
-        TextView result = (TextView) findViewById(R.id.result_text);
-
         if (operationPressed) {
             result.setText("2");
             operationPressed = false;
@@ -418,9 +382,6 @@ public class MainActivity extends AppCompatActivity {
 
     // This function adds a 3 to the currently displayed number
     public void buttonThreePressed(View view) {
-
-        TextView result = (TextView) findViewById(R.id.result_text);
-
         if (operationPressed) {
             result.setText("3");
             operationPressed = false;
@@ -439,9 +400,6 @@ public class MainActivity extends AppCompatActivity {
 
     // This function adds a 4 to the currently displayed number
     public void buttonFourPressed(View view) {
-
-        TextView result = (TextView) findViewById(R.id.result_text);
-
         if (operationPressed) {
             result.setText("4");
             operationPressed = false;
@@ -460,9 +418,6 @@ public class MainActivity extends AppCompatActivity {
 
     // This function adds a 5 to the currently displayed number
     public void buttonFivePressed(View view) {
-
-        TextView result = (TextView) findViewById(R.id.result_text);
-
         if (operationPressed) {
             result.setText("5");
             operationPressed = false;
@@ -481,9 +436,6 @@ public class MainActivity extends AppCompatActivity {
 
     // This function adds a 6 to the currently displayed number
     public void buttonSixPressed(View view) {
-
-        TextView result = (TextView) findViewById(R.id.result_text);
-
         if (operationPressed) {
             result.setText("6");
             operationPressed = false;
@@ -502,9 +454,6 @@ public class MainActivity extends AppCompatActivity {
 
     // This function adds a 7 to the currently displayed number
     public void buttonSevenPressed(View view) {
-
-        TextView result = (TextView) findViewById(R.id.result_text);
-
         if (operationPressed) {
             result.setText("7");
             operationPressed = false;
@@ -523,9 +472,6 @@ public class MainActivity extends AppCompatActivity {
 
     // This function adds an 8 to the currently displayed number
     public void buttonEightPressed(View view) {
-
-        TextView result = (TextView) findViewById(R.id.result_text);
-
         if (operationPressed) {
             result.setText("8");
             operationPressed = false;
@@ -544,8 +490,6 @@ public class MainActivity extends AppCompatActivity {
 
     // This function adds a 9 to the currently displayed number
     public void buttonNinePressed(View view) {
-        TextView result = (TextView) findViewById(R.id.result_text);
-
         if (operationPressed) {
             result.setText("9");
             operationPressed = false;
@@ -559,6 +503,6 @@ public class MainActivity extends AppCompatActivity {
                 result.setText(getDisplayString() + "9");
             }
         }
-
     }
+
 }
